1a4e361c28bae52045a9aa09b3e7d82cf2be1e85
Maksudul Haque <155283184+Maksudulfcub@users.noreply.github.com>
2025-08-15 22:12:35 +0600

    More info showed in country card

--- src/components/City/City.tsx
+++ src/components/City/City.tsx
@@ -1,19 +1,22 @@
 
 export const City = ({ city }) => {
 
-    const { name, country, country_flag } = city;
+    const { name, country, country_flag,weather_summary,tomorrow_weather,local_time } = city;
 
     return (
         <div>
-            <div className="card bg-base-100 lg:w-96 shadow-sm">
+            <div className="card bg-blue-200 lg:w-96 shadow-sm mt-10">
                 <figure>
-                    <img
+                    <img className="p-5"
                         src={country_flag}
-                        alt="Shoes" />
+                        alt="country flag" />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">{name}</h2>
                     <p>Country : {country}</p>
+                    <p className="font-semibold">Today's weather : {weather_summary}</p>
+                    <p className=" text-gray-500">Tomorrow would be : {tomorrow_weather}</p>
+                    <p>Local time : {local_time}</p>
                 </div>
             </div>
         </div>
